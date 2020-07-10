aa4909d7b699cafe8dbb889bc4465711cf801699
Burak Geylani <burakgeylani@hotmail.com>
2020-07-10 03:40:25 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using DovizKuru.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DovizKuru.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            XmlDocument xml = new XmlDocument(); //  yeni bir XML dökümü oluşturuyoruz.
+            xml.Load("http://www.tcmb.gov.tr/kurlar/today.xml"); // bağlantı kuruyoruz.
+            var Tarih_Date_Nodes = xml.SelectSingleNode("//Tarih_Date"); // Count değerini olmak için ana boğumu seçiyoruz.
+            var CurrencyNodes = Tarih_Date_Nodes.SelectNodes("//Currency"); // ana boğum altındaki kur boğumunu seçiyoruz.
+            int CurrencyLength = CurrencyNodes.Count; // toplam kur boğumu sayısını elde ediyor ve for döngüsünde kullanıyoruz.
+
+            List<DovizKuruModel> dovizler = new List<DovizKuruModel>(); // Aşağıda oluşturduğum public class ile bir List oluşturuyoruz.
+            for (int i = 0; i < CurrencyLength; i++) // for u çalıştırıyoruz.
+            {
+                var cn = CurrencyNodes[i]; // kur boğumunu alıyoruz.
+                // Listeye kur bilgirini ekliyoruz.
+                dovizler.Add(new DovizKuruModel
+                {
+                    Kod = cn.Attributes["Kod"].Value,
+                    CrossOrder = cn.Attributes["CrossOrder"].Value,
+                    CurrencyCode = cn.Attributes["CurrencyCode"].Value,
+                    Unit = cn.ChildNodes[0].InnerXml,
+                    Isim = cn.ChildNodes[1].InnerXml,
+                    CurrencyName = cn.ChildNodes[2].InnerXml,
+                    ForexBuying = cn.ChildNodes[3].InnerXml,
+                    ForexSelling = cn.ChildNodes[4].InnerXml,
+                    BanknoteBuying = cn.ChildNodes[5].InnerXml,
+                    BanknoteSelling = cn.ChildNodes[6].InnerXml,
+                    CrossRateOther = cn.ChildNodes[8].InnerXml,
+                    CrossRateUSD = cn.ChildNodes[7].InnerXml,
+                });
+            }
+
+            ViewData["dovizler"] = dovizler; // dovizler List değerini data ya atıyoruz ön tarafta viewbag ile çekeceğiz.
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DovizKuru.MvcApplication" Language="C#" %>
